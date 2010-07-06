4165d9b7f9d28806749c1d5caf914a2dbe9319d4
Tobias Adam <tobias.adam@mindmatters.de>
2010-07-06 22:13:39 +0200

    Basic pattern structure.

---
+++ templates/project/README.md
@@ -0,0 +1,6 @@
+A collection of compass templates and patterns used by mindmatters
+==================================================================
+
+Install the basic project pattern with:
+
+    compass install mm-compass-templates/project --syntax sass
---
+++ templates/project/manifest.rb
@@ -0,0 +1,17 @@
+description "A collection of templates and patterns used by mindmatters."
+
+stylesheet "screen.sass", :media => "screen, projection"
+stylesheet "look/_colors.sass"
+stylesheet "look/_typography.sass"
+stylesheet "layout/_grid.sass"
+stylesheet "layout/_layout_elements.sass"
+
+file "README.md"
+
+help %Q{
+Don’t know where this is shown… Edit me if you find me!
+}
+
+welcome_message %Q{
+Have fun using our templates. Feel free to visit our website at http://www.mindmatters.de
+}
